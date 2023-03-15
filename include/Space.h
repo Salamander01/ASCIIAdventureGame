3bcaabc7af86e1929983a1c71b5152df5cfc43be
Salamander01 <96356159+Salamander01@users.noreply.github.com>
2023-03-15 11:06:40 -0600

    Comment Space.h

--- include/Space.h
+++ include/Space.h
@@ -8,25 +8,30 @@
 
 class Space {
 public:
+    // Constructors
     Space(char symbol, bool passable, std::string name);
     Space(const Space &space);
     Space();
 
-
+    // Destructor (may not actually be needed)
     ~Space();
 
+    // Getters
     char getSymbol();
     bool getPassable();
 
+    // Overloaded operators
     friend std::fstream &operator<< (std::fstream &out, const Space &space);
     friend std::map<char, Space> &operator>> (std::fstream &in, const Space &space);
 
 private:
+    // Attributes
     char symbol;
     bool passable;
 
     std::string name;
 
+    // Friend attribute from Loader.h
     friend std::map<char, Space> spaceTypes; // TODO may or may not work. Test with CLion.
 };
 
