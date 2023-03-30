674cfd46a8270dc279e04973db5fdfad03e8e027
Salamander01 <96356159+Salamander01@users.noreply.github.com>
2023-03-29 19:01:48 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,6 @@
 using namespace std;
 
 int main() {
-    // Simple code for testing purposes once the classes used are implemented.
-//    Level mainLevel = Loader::loadLevel();
-//
-//    IO io;
-//
-//    io.output(mainLevel);
-//
-//    while (true);
 
     cout << "Hello World!";
 
